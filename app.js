b93a56a3b1c0a8c421d8f95795eb5caff90cc706
toukodes <toukodes@gmail.com>
2023-01-14 12:22:55 +0200

    Reading multiple files. Changed to functions. Added test files.

--- app.js
+++ app.js
@@ -1,54 +1,85 @@
-const fs = require('fs');
-const path = require('path')
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
 // message object
 class Message {
-    constructor(sender, receiver, content) {
-        this.sender = sender;
-        this.receiver = receiver;
-        this.content = content;
-    }
+  constructor(sender, receiver, content) {
+    this.sender = sender;
+    this.receiver = receiver;
+    this.content = content;
+  }
 }
 
 // regex for splitting the data
 let timeStampRegex = /\d\d:\d\d:\d\d\s/;
 
-// file to read
-let input = "test.txt";
+// check if folder exists
+const folder = __dirname + "/files/";
+try {
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder);
+  }
+} catch (error) {
+  console.error(error);
+}
 
-// reading the input file in "files" folder
-const data = fs.readFileSync(path.join(__dirname, "/files/", "test.txt"), 'utf-8');
-let dataArr = data.split(timeStampRegex);
+// check files in the folder and assign them to the files variable
+let files = fs.readdirSync(folder);
+console.log(files);
+
+// read the files and put data in dataArr
+let dataArr = [];
+
+function readFiles(files) {
+  let arr = [];
+  files.forEach((file) => {
+    const filepath = path.join(folder, file);
+    console.log("trying to read: ", filepath);
+    const data = fs.readFileSync(filepath, "utf-8");
+    const rawMessages = data.split(timeStampRegex);
+    rawMessages.forEach((msg) => {
+      dataArr.push(msg);
+    });
+    //console.log(dataArr);
+  });
+}
+
+readFiles(files);
 
 // creating array for messages
 
 let msgArr = [];
 
+createMessages(dataArr);
+search();
 // going through the messages in data and pushing them in the msgArr as objects
-
-for (let message of dataArr) {
+function createMessages(dataArr) {
+  for (let message of dataArr) {
     let pieces = message.split("\n");
     let sender = pieces[0].split("  ")[1];
     if (!sender) continue;
     let receiverData = pieces[0].split("  ")[3];
-    let receiver = pieces[0].split("  ")[3].substring(0, receiverData.length - 2);
+    let receiver = pieces[0]
+      .split("  ")[3]
+      .substring(0, receiverData.length - 2);
     let content = pieces[1].substring(1, pieces[1].length - 1);
-    let newMessage = new Message(sender, receiver, content)
+    let newMessage = new Message(sender, receiver, content);
     // only push message in the array if it has content
-    if (newMessage.content != '') msgArr.push(newMessage);
+    if (newMessage.content != "") msgArr.push(newMessage);
+  }
 }
 
 // taking user input for search
-
-readline.question(`What are you looking for?`, query => {
+function search() {
+  readline.question(`What are you looking for?`, (query) => {
     // quitting program if empty query
-    if(query.length < 1) {
-        readline.close()
-        return;
+    if (query.length < 1) {
+      readline.close();
+      return;
     }
     // creating an array for the matching messages
     let messages = [];
@@ -57,11 +88,12 @@ readline.question(`What are you looking for?`, query => {
     console.log(query);
     // looking for matching words in contents of each message
     for (const msg of msgArr) {
-        let message = msg.content;
-        if (message.toLowerCase().match(regex)) {
-            messages.push(msg);
-        }
+      let message = msg.content;
+      if (message.toLowerCase().match(regex)) {
+        messages.push(msg);
+      }
     }
     console.log(messages);
     readline.close();
-})
+  });
+}
